3f36bc1c83f91ce099603cc20b64827ada415d5d
alvinkenyagah <alvin47k@gmail.com>
2023-03-03 00:33:01 +0300

    Add prompt which ask user to enter amount

--- ksh_to_usd.js
+++ ksh_to_usd.js
@@ -1,6 +1,11 @@
 const exchangeRate = 127.60;
 const ksh = process.argv[2];
 
+if (!ksh) {
+    console.log("Please Enter amount in Kenyan shilling to convert to USD.")
+    process.exit();
+}
+
 const usd = (ksh / exchangeRate).toFixed(2);
 
 console.log( `${ksh} KSH is equivalent to $${usd} USD.`);
