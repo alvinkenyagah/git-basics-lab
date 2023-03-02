df62da19b7f087102bb5be4489ba975e4b1f1653
alvinkenyagah <alvin47k@gmail.com>
2023-03-03 00:29:54 +0300

    Disable console.log prompt

--- ksh_to_usd.js
+++ ksh_to_usd.js
@@ -1,11 +1,12 @@
 const exchangeRate = 127.60;
 const ksh = process.argv[2];
 
-if (!ksh) {
-    console.log("kENYAN SHILLING TO USD CONVERTER")
-    console.log("Enter amount in Kenyan shilling: ")
-    process.exit();
-}
+// if (!ksh) {
+//     console.log("kENYAN SHILLING TO USD CONVERTER")
+//     console.log("Enter amount in Kenyan shilling: ")
+//     process.exit();
+// }
 
 const usd = (ksh / exchangeRate).toFixed(2);
 
+console.log( `${ksh} KSH is equivalent to $${usd} USD.`);
