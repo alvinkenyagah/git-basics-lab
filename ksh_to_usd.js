524b165fcb36d36617576332360aaf77bef1f3b5
alvinkenyagah <alvin47k@gmail.com>
2023-03-03 00:30:56 +0300

    Delete consol.og prompt message

--- ksh_to_usd.js
+++ ksh_to_usd.js
@@ -1,12 +1,6 @@
 const exchangeRate = 127.60;
 const ksh = process.argv[2];
 
-// if (!ksh) {
-//     console.log("kENYAN SHILLING TO USD CONVERTER")
-//     console.log("Enter amount in Kenyan shilling: ")
-//     process.exit();
-// }
-
 const usd = (ksh / exchangeRate).toFixed(2);
 
 console.log( `${ksh} KSH is equivalent to $${usd} USD.`);
