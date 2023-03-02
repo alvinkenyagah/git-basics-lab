3ada48f69689c19e6228768101927464584a84c7
alvinkenyagah <alvin47k@gmail.com>
2023-03-03 00:22:30 +0300

    Add console prompt message

--- ksh_to_usd.js
+++ ksh_to_usd.js
@@ -1,3 +1,8 @@
 const exchangeRate = 127.60;
 const ksh = process.argv[2];
 
+if (!ksh) {
+    console.log("kENYAN SHILLING TO USD CONVERTER")
+    console.log("Enter amount in Kenyan shilling: ")
+    process.exit();
+}
